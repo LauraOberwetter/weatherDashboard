9e3c3425d69882c28d4f844ce968b28717e0e757
Laura Oberwetter <leoberwetter@gmail.com>
2021-03-13 17:54:56 -0600

    temp/humidity in 5day forecast

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 var API_KEY = "166a433c57516f51dfab1f7edaed8413";
 
 
-$(document).ready(function() { //prevents js from loading until the document is ready
-    $('.btn').on("click", function() {
+$(document).ready(function () { //prevents js from loading until the document is ready
+    $('.btn').on("click", function () {
         let city = $("#searchValue").val();
         searchCityWeather(city);
-     });
-    function searchCityWeather (city) {
+        forecast(city);
+
+    });
+    function searchCityWeather(city) {
         $.ajax({
             type: "GET",
             url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=imperial`,
             dataType: "json",
-            success: function(data) {
+            success: function (data) {
                 console.log(data);
                 $("#cityResult").text(data["name"]);
                 $("#tempResult").text("Tempreature: " + data["main"]["temp"] + "°F");
@@ -23,18 +25,24 @@ $(document).ready(function() { //prevents js from loading until the document is 
 
         })
     }
-    function forecast (city) {
+    function forecast(city) {
         $.ajax({
             type: "GET",
-            url: `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`,
+            url: `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&appid=${API_KEY}&units=imperial`,
             dataType: "json",
-            success: function(data) {
+            success: function (data) {
                 console.log(data);
-            }
+                for (var i = 0; i < 5; i++) {
+                    $("#temp" + i).text("TEMP: " + data["list"][i]["temp"]["day"] + "°F");
+                    $("#hum" + i).text("HUMIDITY: " + data["list"][i]["humidity"] + "%");
+                    $("#icon" + i).val(data["list"][i]["weather"][0]["icon"]);
 
+                }
+
+            }
         })
     }
 
-  
+
 
 });
